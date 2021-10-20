4f052f67ac85394d82e0dc38bb8841bc51d83fb6
prajapatilalit <prajapatilalit150992@gmail.com>
2021-10-20 21:22:59 +0530

    create and update home page

---
+++ trivia-quiz-app/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="home_container">
+        <h1>Welcome To Trivia Quiz</h1>
+        <button>Start Quiz</button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
